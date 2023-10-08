b9ace0ec05444e93979e0a0638f3b4664b51716a
Rafael Ferreira Mendes <sevendays20@hotmail.com>
2023-10-07 23:27:38 -0300

    update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <nav className="App-nav">
           <ul className="App-navlist">
-            <li><a className="App-link" >Home</a></li>
-            <li><a className="App-link" >Experiências</a></li>
-            <li><a className="App-link" >Projetos</a></li>
-            <li><a className="App-link" >Competências</a></li>
-            <li><a className="App-link" >Contatos</a></li>
+            <li><a href="#" className="App-link" >Home</a></li>
+            <li><a href="#" className="App-link" >Experiências</a></li>
+            <li><a href="#" className="App-link" >Projetos</a></li>
+            <li><a href="#" className="App-link" >Competências</a></li>
+            <li><a href="#" className="App-link" >Contatos</a></li>
           </ul>
         </nav>
         <img src={logo} className="App-logo" alt="logo" />
